843f50c12ff35250dfe29f4447ecab563754c28f
Rend <cainhurst1@users.noreply.github.com>
2023-10-09 21:34:59 +0530

    removed redundant code

--- src/main/java/com/task/todolist/telegramcontroller/DisplayEventsConversation.java
+++ src/main/java/com/task/todolist/telegramcontroller/DisplayEventsConversation.java
@@ -4,12 +4,7 @@ import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventDateTime;
 import com.task.todolist.model.GoogleCalendarService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
-
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
